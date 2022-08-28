099b1179b60c82312777baedec3ad67e4af62a0d
bopscka <rebeccatous98@gmail.com>
2022-08-28 17:53:31 -0400

    script de base de la fonction ProposerNombre, sans affichage de l'hitorique des tentatives, juste recuperer le nombre proposé et le comparer avec le nombre à trouver puis afficher soit un guide ou Bravogit status dans la console

--- script.js
+++ script.js
@@ -7,6 +7,34 @@ const genererNombreAleatoire = () =>{
     return nombre
 }
 
+const nombreATrouver = genererNombreAleatoire()
+console.log(nombreATrouver)
+
+const proposerNombre = () => {
+    // recuperer le champs avec le nombre 
+    const input = document.getElementById('input-nombre')
+    const valeur = input.value
+
+    console.log(valeur, typeof valeur)
+
+    //transformer la valeur texte en nombre
+    const nombrePropose = parseInt(valeur,10)
+    
+    console.log(nombrePropose, typeof nombrePropose)
+
+    // comparer le nombre avec le nombre a trouver
+    if (nombrePropose === nombreATrouver){
+        console.log('BRAVO!!!!')
+    }else{
+        if(nombreATrouver > nombrePropose){
+            console.log('le nombre est plus grand ')
+        }else{
+            console.log('le nombre est plus petit')
+        }
+    }
+
+}
+
 // Récupérer le bouton
 // lorsqu'il y a u click; effectuer une proposition de nombre
 
